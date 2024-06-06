dfbef4e95a6bfd411703e0570e5e960f0920e145
lidiamiceli <miceli.lidia1996@gmail.com>
2024-06-06 16:36:27 +0200

    commit

--- 06-06-24/index.js
+++ 06-06-24/index.js
@@ -11,25 +11,25 @@ function fetchProducts() {
         const productsContainer = document.getElementById('products-container');
         data.forEach(product => {
     
-          const card = document.createElement('div'); //creo una card
+          const card = document.createElement('div'); 
           card.className = 'card';
   
          
-          const title = document.createElement('h2'); //creo un titolo
+          const title = document.createElement('h2'); 
           title.className = 'card-title';
           title.textContent = product.title;
   
 
           const price = document.createElement('p');
           price.className = 'card-price';
-          price.textContent = `$${product.price}`;  // creo il prezzo
+          price.textContent = `$${product.price}`;  
   
           
-          card.appendChild(title); // mostro title e price nella card
+          card.appendChild(title); 
           card.appendChild(price); 
   
 
-          productsContainer.appendChild(card); //mostro le card del container
+          productsContainer.appendChild(card); 
         });
       })
 
