5833793eb58ee471a4521753f12df05d4c13341c
lidiamiceli <miceli.lidia1996@gmail.com>
2024-06-25 09:43:05 +0200

    commit

--- 24-06-24/index.js
+++ 24-06-24/index.js
@@ -12,13 +12,13 @@ window.addEventListener("DOMContentLoaded", async () => {
       const sidebar = getSidebarHTML();
       const footer = getFooterHTML();
 
-      // Attendere il completamento di getMainHTML() per ottenere il markup HTML degli articoli
+      
       const mainHTML = await getMainHTML();
 
-      // Concatenare il markup HTML in un'unica stringa
+      
       const html = header + sidebar + mainHTML + footer;
 
-      // Inserire il markup HTML nella pagina
+      
       appEl.innerHTML = html;
     } catch (error) {
       console.error('Error rendering page:', error);
